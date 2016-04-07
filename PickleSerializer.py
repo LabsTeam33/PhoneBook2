5e1d36d0072a118fd59137ffb3404f8651fbffcc
Supremist <sergkarv@gmail.com>
2016-04-07 20:40:29 +0300

    Add docstring to PickleSerializer

--- PickleSerializer.py
+++ PickleSerializer.py
@@ -6,6 +6,11 @@ class PickleSerializer:
 
     @staticmethod
     def load(file_path):
+        """
+        Load serialized data from file_path file
+        :param file_path: path to data file
+        :return: if success return loaded data, else None
+        """
         try:
             with open(file_path, 'rb') as file:
                 return pickle.load(file)
@@ -14,5 +19,11 @@ class PickleSerializer:
 
     @staticmethod
     def save(file_path, data):
+        """
+        Serialize data to file_path file. File will be rewrite.
+        :param file_path: path to data file
+        :param data: serializable object
+        :return: None
+        """
         with open(file_path, 'wb') as file:
             pickle.dump(data, file)
