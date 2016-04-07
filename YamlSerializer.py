fc3301cf54c3f83c9401980a3fbd5b2e61496588
YuriiHrushko <yura1996hrushko@gmail.com>
2016-04-07 22:43:52 +0300

    Used pep8

--- YamlSerializer.py
+++ YamlSerializer.py
@@ -6,7 +6,7 @@ class YamlSerializer:
 
     @staticmethod
     def load(file_path):
-	"""
+        """
         Load serialized data from file_path file
         :param file_path: path to data file
         :return: if success return loaded data, else None
@@ -19,7 +19,7 @@ class YamlSerializer:
 
     @staticmethod
     def save(file_path, data):
-	"""
+        """
         Serialize data to file_path file. File will be rewrite.
         :param file_path: path to data file
         :param data: serializable object
