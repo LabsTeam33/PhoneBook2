e62765a314933c549c65ee32648511e406e638c6
YuriiHrushko <yura1996hrushko@gmail.com>
2016-04-07 22:14:04 +0300

    Add docstring to YamlSerializer

--- YamlSerializer.py
+++ YamlSerializer.py
@@ -6,6 +6,11 @@ class YamlSerializer:
 
     @staticmethod
     def load(file_path):
+	"""
+        Load serialized data from file_path file
+        :param file_path: path to data file
+        :return: if success return loaded data, else None
+        """
         try:
             with open(file_path, 'rb') as file:
                 return yaml.load(file)
@@ -14,5 +19,11 @@ class YamlSerializer:
 
     @staticmethod
     def save(file_path, data):
+	"""
+        Serialize data to file_path file. File will be rewrite.
+        :param file_path: path to data file
+        :param data: serializable object
+        :return: None
+        """
         with open(file_path, 'wb') as file:
             yaml.dump(data, file)
